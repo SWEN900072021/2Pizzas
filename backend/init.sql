6a958920a640dff3a3d42356a9dc7eabea0f24a3
Dini <mahardini.rizky@gmail.com>
2021-08-28 14:38:14 +1000

    First reset db action

---
+++ backend/init.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE cool_guys;
+INSERT INTO cool_guys (very_cool, coolness) VALUES ('hello', 'world');INSERT INTO cool_guys (very_cool, coolness) VALUES ('hello', 'world');
+INSERT INTO cool_guys (very_cool, coolness) VALUES ('yessir', 'yesmaam');
