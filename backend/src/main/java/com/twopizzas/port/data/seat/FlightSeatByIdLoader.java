fe367fdef7284f6f62f00ce2e2b1806612d0ae25
maxplumley <maxplumley@users.noreply.github.com>
2021-09-04 20:53:22 +1000

    Update FlightSeatByIdLoader.java, FlightSeatAllocationMapper.java, and FlightSeatAllocationMapperImpl.java

---
+++ backend/src/main/java/com/twopizzas/port/data/seat/FlightSeatByIdLoader.java
@@ -0,0 +1,28 @@
+package com.twopizzas.port.data.seat;
+
+import com.twopizzas.data.BaseValueHolder;
+import com.twopizzas.data.ValueHolder;
+import com.twopizzas.data.ValueLoader;
+import com.twopizzas.domain.EntityId;
+import com.twopizzas.domain.flight.FlightSeat;
+import com.twopizzas.util.AssertionConcern;
+
+public class FlightSeatByIdLoader extends AssertionConcern implements ValueLoader<FlightSeat> {
+
+    private final FlightSeatMapper mapper;
+    private final EntityId seatId;
+
+    public FlightSeatByIdLoader(FlightSeatMapper mapper, EntityId seatId) {
+        this.mapper = notNull(mapper, "mapper");
+        this.seatId = notNull(seatId, "seatId");
+    }
+
+    public FlightSeatByIdLoader(FlightSeatMapper mapper, String seatId) {
+        this(mapper, EntityId.of(seatId));
+    }
+
+    @Override
+    public ValueHolder<FlightSeat> load() {
+        return new BaseValueHolder<>(mapper.read(seatId));
+    }
+}
--- backend/src/main/java/com/twopizzas/port/data/seatallocation/FlightSeatAllocationMapper.java
+++ backend/src/main/java/com/twopizzas/port/data/seatallocation/FlightSeatAllocationMapper.java
@@ -3,8 +3,9 @@ package com.twopizzas.port.data.seatallocation;
 import com.twopizzas.data.DataMapper;
 import com.twopizzas.domain.EntityId;
 import com.twopizzas.domain.flight.FlightSeatAllocation;
+import com.twopizzas.port.data.SqlResultSetMapper;
 
-public interface FlightSeatAllocationMapper extends DataMapper<FlightSeatAllocation, EntityId, FlightSeatAllocationSpecification> {
+public interface FlightSeatAllocationMapper extends DataMapper<FlightSeatAllocation, EntityId, FlightSeatAllocationSpecification>, SqlResultSetMapper<FlightSeatAllocation> {
     @Override
     default Class<FlightSeatAllocation> getEntityClass() {
         return FlightSeatAllocation.class;
