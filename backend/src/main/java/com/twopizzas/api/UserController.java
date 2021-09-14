6d4d7566d0bc9dcde9b9c8e048925c3f914d3066
Ben <bennguyen96@gmail.com>
2021-09-14 20:53:16 +1000

    added user controller signup route

---
+++ backend/src/main/java/com/twopizzas/api/UserController.java
@@ -0,0 +1,35 @@
+package com.twopizzas.api;
+
+import com.twopizzas.auth.AuthenticationProvider;
+import com.twopizzas.di.Autowired;
+import com.twopizzas.di.Controller;
+import com.twopizzas.domain.user.Customer;
+import com.twopizzas.domain.user.User;
+import com.twopizzas.domain.user.UserRepository;
+import com.twopizzas.web.HttpMethod;
+import com.twopizzas.web.RequestBody;
+import com.twopizzas.web.RequestMapping;
+import com.twopizzas.web.RestResponse;
+
+import java.util.Optional;
+
+@Controller
+public class UserController {
+    private final UserRepository userRepository;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public UserController(UserRepository userRepository, AuthenticationProvider authenticationProvider) {
+        this.userRepository = userRepository;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @RequestMapping(path = "/signup", method = HttpMethod.POST)
+    RestResponse<SignupResponseDTO> createUser(@RequestBody SignupRequestDTO body) {
+        User user = new Customer(body.getUsername(), body.getPassword(),body.getGivenName(), body.getSurname(), body.getEmail());
+        userRepository.save(user);
+        String token = authenticationProvider.login(user.getUsername(), user.getPassword()).orElseThrow(() -> new RuntimeException("Couldn't get token"));
+        return RestResponse.ok(new SignupResponseDTO().setToken(token));
+    }
+
+}
