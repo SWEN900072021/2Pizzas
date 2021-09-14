93b0ea5abd8970449ded1ef68f22a53232dae513
maxplumley <maxplumley@users.noreply.github.com>
2021-09-14 21:36:51 +1000

    Update UserController.java, UserRepositoryImpl.java, and WebApplicationContextImpl.java

--- backend/src/main/java/com/twopizzas/port/data/user/UserRepositoryImpl.java
+++ backend/src/main/java/com/twopizzas/port/data/user/UserRepositoryImpl.java
@@ -3,19 +3,25 @@ package com.twopizzas.port.data.user;
 import com.twopizzas.di.Autowired;
 import com.twopizzas.di.Component;
 import com.twopizzas.domain.user.User;
+import com.twopizzas.domain.user.UserRepository;
 import com.twopizzas.port.data.AbstractRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
-public class UserRepositoryImpl extends AbstractRepository<User, UserSpecification, UserMapper> {
+public class UserRepositoryImpl extends AbstractRepository<User, UserSpecification, UserMapper> implements UserRepository {
 
     @Autowired
     public UserRepositoryImpl(UserMapper dataMapper) {
         super(dataMapper);
     }
 
-    List<User> find(String username, String password) {
-        return doSpecification(new UserByUsernameAndPasswordSpecification(dataMapper, username, password));
+    public Optional<User> find(String username, String password) {
+        List<User> users = doSpecification(new UserByUsernameAndPasswordSpecification(dataMapper, username, password));
+        if (users.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(users.get(0));
     }
 }
