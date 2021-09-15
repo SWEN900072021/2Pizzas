47e3eecd84b5895647bf880f49e4741421edd9e5
maxplumley <maxplumley@users.noreply.github.com>
2021-09-16 08:18:17 +1000

    Update HttpRequestDispatcherImpl.java

--- backend/src/main/java/com/twopizzas/web/HttpRequestDispatcherImpl.java
+++ backend/src/main/java/com/twopizzas/web/HttpRequestDispatcherImpl.java
@@ -37,7 +37,7 @@ public class HttpRequestDispatcherImpl implements HttpRequestDispatcher {
             Map<String, String> headers = new HashMap<>();
             headers.put("Access-Control-Allow-Origin", request.getHeaders().getOrDefault("origin", "*"));
             headers.put("Access-Control-Allow-Methods", delegates.stream().flatMap(d -> d.getMethods().stream()).map(HttpMethod::name).distinct().collect(Collectors.joining(", ")));
-            headers.put("Access-Control-Allow-Headers", request.getHeaders().getOrDefault("access-control-request-headers", "*")));
+            headers.put("Access-Control-Allow-Headers", request.getHeaders().getOrDefault("access-control-request-headers", "*"));
             return new HttpResponse(
                     HttpStatus.NO_CONTENT, null, headers
             );
