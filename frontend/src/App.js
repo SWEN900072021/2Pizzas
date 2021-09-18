fdf4e629d054f4a06efe54ae6c9f8bb50ef48a73
sothbak <sothearoth.bak.s@gmail.com>
2021-09-18 19:59:34 +1000

    wip dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import Login from './containers/Login'
 import Home from './containers/Home'
 import Signup from './containers/Signup'
+import UserDashboard from './containers/UserDashboard'
 
 function App() {
   const queryClient = new QueryClient()
@@ -16,6 +17,7 @@ function App() {
           <Route exact path='/' render={() => <Home />} />
           <Route exact path='/login' render={() => <Login />} />
           <Route exact path='/signup' render={() => <Signup />} />
+          <Route exact path='/dashboard' render={() => <UserDashboard />} />
         </Switch>
       </main>
     </QueryClientProvider>
