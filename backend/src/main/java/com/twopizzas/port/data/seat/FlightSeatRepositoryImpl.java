5e03d187ea638982f0f926705a40ab535af8d3bc
maxplumley <maxplumley@users.noreply.github.com>
2021-09-25 14:27:27 +1000

    Update FlightSeatRepositoryImpl.java, FlightSeatRepositoryImpl.java, and 7 more files...

--- backend/src/main/java/com/twopizzas/port/data/seat/FlightSeatRepositoryImpl.java
+++ backend/src/main/java/com/twopizzas/port/data/seat/FlightSeatRepositoryImpl.java
@@ -1,12 +1,10 @@
-package com.twopizzas.port.data.flight;
+package com.twopizzas.port.data.seat;
 
 import com.twopizzas.di.Autowired;
 import com.twopizzas.di.Component;
 import com.twopizzas.domain.flight.FlightSeat;
 import com.twopizzas.domain.flight.FlightSeatRepository;
 import com.twopizzas.port.data.AbstractRepository;
-import com.twopizzas.port.data.seat.FlightSeatMapper;
-import com.twopizzas.port.data.seat.FlightSeatSpecification;
 
 @Component
 public class FlightSeatRepositoryImpl extends AbstractRepository<FlightSeat, FlightSeatSpecification, FlightSeatMapper> implements FlightSeatRepository {
